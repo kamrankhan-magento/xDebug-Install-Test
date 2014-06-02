8110da48ab0fb5785c01d3ff4936cf481b1f60fc
Zain <zainengineer@mrphp.com.au>
2014-06-02 15:29:41 +0930

    simple bug fix
    now check

--- checks/first_check.php
+++ checks/first_check.php
@@ -1,8 +1,8 @@
 <?php
 if (function_exists('xdebug_is_enabled')){
     if (xdebug_is_enabled()){
-        echo "it seems xdebug is installed and enabled \n";
-        xdebug_debug_zval()
+        echo "it seems xdebug is installed and enabled showing memory usage is \n";
+        echo number_format(xdebug_memory_usage());
     }
     else{
         echo "xdebug is installed but not enabled \n";
